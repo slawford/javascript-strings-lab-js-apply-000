2eaf794740b3d91040564da8d3f805228217f47c
Savannah Lawford <savannahroselawford@gmail.com>
2017-12-07 23:09:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson!"
 
-var greetSpecialGuest = "Hello, "  + "Neil deGrasse Tyson!!";
+var greetSpecialGuest = "Hello, "  + specialGuest + "!";
 
 var topic = "Let's talk about space.";
 
 var conversation = `${topic}`;
+
+var pets = "dog"
